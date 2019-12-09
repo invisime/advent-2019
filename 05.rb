247b70893d7ba5b14e103ca386f7178ddd095802
Thom Root <tlroot@sep.com>
2019-12-09 07:03:34 -0500

    updated day 5 for consistency

--- 05.rb
+++ 05.rb
@@ -2,9 +2,12 @@
 
 require_relative 'intcode'
 
+puts "('1' for Part 1 or '5' for Part 2)"
 diagnostic = File.read('input05.txt').split(',').map(&:to_i)
-execute diagnostic, [1]
-puts 'Was it 2845163?'
+execute diagnostic
 
-execute diagnostic, [5]
-puts 'Was it 9436229?'
+# For 1
+# puts 'Was it a bunch of 0s then a 2845163?'
+
+# For 5
+# puts 'Was it 9436229?'
