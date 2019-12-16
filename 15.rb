aa5978d329ffe022483fbefd3224094979d96047
Thom Root <tlroot@sep.com>
2019-12-16 00:13:07 -0500

    Day 15 Part 2

--- 15.rb
+++ 15.rb
@@ -4,33 +4,36 @@ require_relative 'intcode'
 require 'io/console'
 require 'pry'
 
+WALL = 0
+EMPTY = 1
+OXYGEN = 2
+NORTH = 'w'
+WEST = 'a'
+SOUTH = 's'
+EAST = 'd'
+COMPASS = [NORTH, EAST, SOUTH, WEST]
+
 class Robot
 
-  WALL = 0
-  EMPTY = 1
-  OXYGEN = 2
-  NORTH = 'w'
-  WEST = 'a'
-  SOUTH = 's'
-  EAST = 'd'
-  COMPASS = [NORTH, EAST, SOUTH, WEST]
-
-  attr_reader :x, :y, :facing, :explored
+  attr_reader :x, :y, :facing, :visited, :could_visit
 
   def initialize free_play=false
     @brain = IntcodeComputer.from_file "input15.txt"
     @brain.run
     @x, @y, @facing = 0, 0, NORTH
-    @explored = {}
+    @visited, @could_visit = [], []
   end
 
   def direction keypress
     valid_direction = ' wsad'.index keypress
-    raise "Invalid direction #{keypress}" unless valid_direction
-    valid_direction
+    valid_direction || nil
   end
 
-  def new_location wasd
+  def know_about
+    @visited | @could_visit
+  end
+
+  def neighbor wasd
     case wasd
     when NORTH
       { x: @x, y: @y + 1 }
@@ -43,21 +46,89 @@ class Robot
     end
   end
 
+  def neighbors
+    COMPASS.map {|direction| neighbor direction}
+  end
+
+  def opposite wasd
+    COMPASS[(COMPASS.index(wasd) + 2) % 4]
+  end
+
   def step wasd
     @facing = wasd
-    location = new_location(wasd)
-    return WALL if @explored[location] == WALL
+    location = neighbor(wasd)
+    return WALL if @know_about[location] == WALL
 
-    @brain.queue_input direction(wasd)
+    command = direction(wasd)
+    return unless command
+    @brain.queue_input command
     @brain.run
-    
-    @explored[location] = type = @brain.outputs.shift
+
+    @visited[location] = type = @brain.outputs.shift
     unless type == WALL
-      @x, @y = location[:x], location[:y] 
+      @x, @y = location[:x], location[:y]
     end
     type
   end
 
+  def explore
+    new_options = peek!
+    if new_options.length == 1
+      step new_options.first
+      return 1 + explore
+    end
+    return 0
+  end
+
+  def peek!
+    original_facing = @facing
+    choices = COMPASS.reduce([]) do |new_empty_directions, wasd|
+      next new_empty_directions if @know_about.include? neighbor(wasd)
+      type = step wasd
+      unless type == WALL
+        step opposite(wasd)
+        new_empty_directions << wasd
+      end
+      new_empty_directions
+    end
+    @facing = original_facing
+    choices
+  end
+
+  # def peek
+  #   peek!.each {|direction| @visited.delete(neighbor direction)}
+  # end
+
+  def interactive_mode
+    print `clear`
+    feedback = ""
+    peek
+    @visited 
+    loop do
+      puts "\e[3;0H"
+      MindVisualizer.display self
+      puts feedback
+      puts "Enter spacebar (to let it explore on its own), W, A, S, D, or ESC."
+      keypress = STDIN.getch.downcase
+      case keypress
+      when "\e"
+        puts `clear`, "Goodbye!"
+        exit
+      when '/'
+        binding.pry
+      when ' '
+        steps = explore
+        feedback = "I went #{steps} steps before I found a fork in the road."
+      when /[wasd]/
+        returning = @visited.include? neighbor(keypress)
+        step keypress
+        peek unless returning
+      end
+    end
+  end
+end
+
+class MindVisualizer
   TILE = {
     WALL => '#',
     EMPTY => '.',
@@ -70,16 +141,16 @@ class Robot
     SOUTH => 'v',
     EAST => '>'
   }
-  def render
+  def self.display robot
     display = ""
     40.downto(0).each do |iy|
       y = iy - 20
       0.upto(80).each do |ix|
         x = ix - 40
-        if y == @y && x == @x
-          display += FACING[@facing]
+        if y == robot.y && x == robot.x
+          display += FACING[robot.facing]
         else
-          display += TILE[@explored[{x: x, y: y}]]
+          display += TILE[robot.visited[{x: x, y: y}]]
         end
       end
       display += "\n"
@@ -89,28 +160,6 @@ class Robot
 end
 
 if __FILE__ == $0
-  # step_through = ARGV.include?("slowly")
-  # display = step_through || ARGV.include?("show-me-your-moves")
-  instructions = "Enter W, A, S, D, or ESC:"
   # Part 1
-  print "\e[3;0H"
-  puts instructions
-  robot = Robot.new
-  loop do
-    keypress = STDIN.getch.downcase
-    
-    if keypress == "\e"
-      puts "", "Goodbye!"
-      exit
-    end
-
-    if keypress == '/'
-      binding.pry
-    else
-      type = robot.step keypress
-      robot.render
-      print "\e[3;0H"
-      puts instructions
-    end
-  end
+  Robot.new.interactive_mode
 end
---
+++ blind.rb
@@ -0,0 +1,138 @@
+#!/usr/bin/env ruby
+
+require_relative 'intcode'
+require 'io/console'
+require 'pry'
+
+WALL = 0
+EMPTY = 1
+OXYGEN = 2
+NORTH = 1
+SOUTH = 2
+WEST = 3
+EAST = 4
+COMPASS = [NORTH, EAST, SOUTH, WEST]
+READABLE = [nil, "NORTH", "WEST", "SOUTH", "EAST"]
+
+class Coordinates
+  attr_reader :x, :y
+
+  def initialize x, y
+    @x, @y = x, y
+  end
+
+  def neighbor direction
+    x, y = case direction
+    when NORTH
+      [@x, @y + 1]
+    when WEST
+      [@x - 1, @y]
+    when SOUTH
+      [@x, @y - 1]
+    when EAST
+      [@x + 1, @y]
+    end
+    Coordinates.new x, y
+  end
+
+  def to_s
+    "#@x, #@y"
+  end
+
+  def == other
+    to_s == other.to_s
+  end
+end
+
+class Robot
+
+  attr_reader :location
+
+  def initialize
+    @location = Coordinates.new 0, 0
+    @env = IntcodeComputer.from_file "input15.txt"
+    @env.run
+  end
+
+  def step direction
+    @env.queue_input direction
+    @env.run
+    type = @env.outputs.shift
+    @location = @location.neighbor(direction) unless type == WALL
+    type
+  end
+end
+
+class Node
+  attr_reader :body, :address, :parent, :type, :direction_from_home, :children
+
+  def initialize body, address, parent, type, direction_from_home
+    @body = body
+    @address = address
+    @type = type
+    @parent = parent
+    @direction_from_home = direction_from_home
+    @children = {}
+    raise "Something went wrong" unless @body.location == @address
+  end
+
+  def self.origin body
+    origin = Node.new body, Coordinates.new(0, 0), nil, [], nil
+  end
+
+  def follow path
+    return self if path.empty?
+    @children[path[0]].follow path[1..-1]
+  end
+
+  def path_to_oxygen seen_addresses=[]
+    seen_addresses << @address
+    return [] if @type == OXYGEN
+    COMPASS.each do |direction|
+      raise "Something went wrong" unless @body.location == @address
+      next if seen_addresses.include? @address.neighbor(direction)
+      child = go_to_child(direction)
+      next unless child
+      if path = child.path_to_oxygen(seen_addresses)
+        return [direction] + path
+      end
+    end
+    @body.step direction_to_home
+    return false
+  end
+
+  def path_to_farthest_point seen_addresses=[]
+    seen_addresses << @address
+    longest_path = []
+    COMPASS.each do |direction|
+      raise "Something went wrong" unless @body.location == @address
+      next if seen_addresses.include? @address.neighbor(direction)
+      child = go_to_child(direction)
+      next unless child
+      path = [direction] + child.path_to_farthest_point(seen_addresses)
+      longest_path = path if path.length > longest_path.length
+    end
+    @body.step direction_to_home
+    longest_path
+  end
+
+  def go_to_child direction
+    child_type = @body.step(direction)
+    return false if child_type == WALL
+    @children[direction] = Node.new @body, @address.neighbor(direction), self, child_type, direction
+  end
+
+  def direction_to_home
+    COMPASS[(COMPASS.index(@direction_from_home) + 2) % 4]
+  end
+end
+
+if __FILE__ == $0
+  # Part 1
+  robot = Robot.new
+  origin = Node.origin robot
+  path_to_oxygen = origin.path_to_oxygen
+  oxygen = origin.follow path_to_oxygen
+  longest_path = oxygen.path_to_farthest_point
+  binding.pry
+end
